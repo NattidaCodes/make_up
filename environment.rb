87cb443db8310d9d58643d782ddd03c6312bfc93
Nattida Wong <nattidac.wong@gmail.com>
2020-10-28 14:24:52 +0000

    Named Classes and Environment Setup

---
+++ environment.rb
@@ -0,0 +1,9 @@
+require_relative "./lib/api"
+require_relative "./lib/cli"
+require_relative "./lib/lipstick"
+
+
+require 'pry'
+require '' #gem run 'bundle install'
+require 'met/http' #requests from api
+require 'json' #allows to parse data from api
