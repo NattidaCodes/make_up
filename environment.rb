9dedc874eb49330840014af4e04bbe90082ac6b7
Nattida Wong <nattidac.wong@gmail.com>
2020-10-29 02:32:39 +0000

    def fetch_makeup

--- environment.rb
+++ environment.rb
@@ -2,6 +2,7 @@ require_relative "./lib/api"
 require_relative "./lib/cli"
 require_relative "./lib/lipstick"
 
+
 require 'pry'
 require 'net/http' #requests from api
 require 'json' #allows to parse data from api
--- lib/api.rb
+++ lib/api.rb
@@ -1,4 +1,12 @@
 class API 
   
+  def self.fetch_makeup
+    url = "http://makeup-api.herokuapp.com/api/v1/products.json"
+    binding.pry
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    hash = JSON.parse(response)
+  end
+  
 end
   
